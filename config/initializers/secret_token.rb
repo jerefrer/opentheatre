04a9324dec958ac65478792d0f52ec567b7d453b
Jérémy FRERE <frere.jeremy@gmail.com>
2013-11-17 11:13:33 +0100

    Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Opentheatre::Application.config.secret_key_base = 'bd83d12b75b7e95631ef8a8d68d54e3dfec14b08897861365eba5edd146a418dcd84e0695591bf3739e94399c4117aad908834d4ed44fe4797e6a2b7975504d4'
