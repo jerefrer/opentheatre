a3a00c330b6f85d14ddc5789f297146fd7262395
Jérémy FRERE <frere.jeremy@gmail.com>
2013-11-24 17:39:59 +0100

    Refactors image_preview_input for simple form (to reduce complexity)

--- app/inputs/image_preview_input.rb
+++ app/inputs/image_preview_input.rb
@@ -1,12 +1,20 @@
 class ImagePreviewInput < SimpleForm::Inputs::FileInput
   def input
-    version = input_html_options.delete(:preview_version)
-    version ||= :thumb
+    (image_tag_if_present << @builder.file_field(attribute_name, input_html_options)).html_safe
+  end
+
+private
+
+  def image_tag_if_present
     out = ''
     if object.send("#{attribute_name}?")
       out << template.image_tag(object.send(attribute_name).tap {|o| break o.send(version) if version}.send('url'))
+      out << ' '
     end
-    out << ' '
-    (out << @builder.file_field(attribute_name, input_html_options)).html_safe
+    out
+  end
+
+  def version
+    input_html_options.delete(:preview_version) || :thumb
   end
 end
